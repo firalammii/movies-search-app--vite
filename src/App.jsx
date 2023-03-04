0943d05d4f3d668742e9e9fc8c5c174180d3ab9f
firalammii <90164296+firalammii@users.noreply.github.com>
2023-03-05 00:55:27 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 import SeachBar from './components/SeachBar';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
 
   return (
     <div className="App">
